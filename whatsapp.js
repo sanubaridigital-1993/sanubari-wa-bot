0fe7dc28973f80a1e355fd1d0d6b0684ed2edb7b
sanubaridigital-1993 <sanubaridigital@gmail.com>
2026-03-11 11:36:16 +0700

    Update whatsapp.js

--- whatsapp.js
+++ whatsapp.js
@@ -1,4 +1,4 @@
-const { default: makeWASocket, useMultiFileAuthState } = require("@whiskeysockets/baileys")
+const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = require("@whiskeysockets/baileys")
 const pino = require("pino")
 const qrcode = require("qrcode-terminal")
 
@@ -8,24 +8,33 @@ async function startWhatsApp(){
 
     const sock = makeWASocket({
         logger: pino({ level: "silent" }),
-        auth: state
+        auth: state,
+        browser: ["Sanubari Bot","Chrome","1.0"]
     })
 
     sock.ev.on("creds.update", saveCreds)
 
-    sock.ev.on("connection.update", ({ connection, qr }) => {
+    sock.ev.on("connection.update", ({ connection, qr, lastDisconnect }) => {
 
         if(qr){
-            console.log("SCAN QR WHATSAPP DI BAWAH:")
+            console.log("SCAN QR DI BAWAH INI")
             qrcode.generate(qr, { small: true })
         }
 
         if(connection === "open"){
-            console.log("WHATSAPP BERHASIL TERHUBUNG")
+            console.log("WHATSAPP CONNECTED")
         }
 
         if(connection === "close"){
             console.log("WHATSAPP DISCONNECTED")
+
+            const shouldReconnect =
+                lastDisconnect?.error?.output?.statusCode !== DisconnectReason.loggedOut
+
+            if(shouldReconnect){
+                console.log("RECONNECTING...")
+                startWhatsApp()
+            }
         }
 
     })
