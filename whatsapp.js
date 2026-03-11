8a8458c2701af9b0c4a09a665ea286d4c24c7022
sanubaridigital-1993 <sanubaridigital@gmail.com>
2026-03-11 11:32:35 +0700

    Update whatsapp.js

--- whatsapp.js
+++ whatsapp.js
@@ -1,32 +1,36 @@
 const { default: makeWASocket, useMultiFileAuthState } = require("@whiskeysockets/baileys")
-const qrcode = require("qrcode-terminal")
 const pino = require("pino")
+const qrcode = require("qrcode-terminal")
 
 async function startWhatsApp(){
 
-const { state, saveCreds } = await useMultiFileAuthState("./session")
+    const { state, saveCreds } = await useMultiFileAuthState("./session")
 
-const sock = makeWASocket({
-logger: pino({ level: "silent" }),
-auth: state
-})
+    const sock = makeWASocket({
+        logger: pino({ level: "silent" }),
+        auth: state
+    })
 
-sock.ev.on("creds.update", saveCreds)
+    sock.ev.on("creds.update", saveCreds)
 
-sock.ev.on("connection.update", ({ connection, qr }) => {
+    sock.ev.on("connection.update", ({ connection, qr }) => {
 
-if(qr){
-qrcode.generate(qr, { small: true })
-}
+        if(qr){
+            console.log("SCAN QR WHATSAPP DI BAWAH:")
+            qrcode.generate(qr, { small: true })
+        }
 
-if(connection === "open"){
-console.log("WHATSAPP CONNECTED")
-}
+        if(connection === "open"){
+            console.log("WHATSAPP BERHASIL TERHUBUNG")
+        }
 
-})
+        if(connection === "close"){
+            console.log("WHATSAPP DISCONNECTED")
+        }
 
-return sock
+    })
 
+    return sock
 }
 
 module.exports = startWhatsApp
