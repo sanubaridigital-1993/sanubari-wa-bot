a23c96595befc5e8c25eb19ab56d4f93b018c2a5
sanubaridigital-1993 <sanubaridigital@gmail.com>
2026-03-11 11:43:57 +0700

    Update whatsapp.js

--- whatsapp.js
+++ whatsapp.js
@@ -3,38 +3,29 @@ const pino = require("pino")
 
 async function startWhatsApp(){
 
-const { state, saveCreds } = await useMultiFileAuthState("./session")
+    const { state, saveCreds } = await useMultiFileAuthState("./session")
 
-const sock = makeWASocket({
-logger: pino({ level: "silent" }),
-auth: state,
-browser: ["SanubariBot","Chrome","1.0"]
-})
+    const sock = makeWASocket({
+        logger: pino({ level: "silent" }),
+        auth: state,
+        browser: ["SanubariBot","Chrome","1.0"]
+    })
 
-sock.ev.on("creds.update", saveCreds)
+    sock.ev.on("creds.update", saveCreds)
 
-sock.ev.on("connection.update", async ({ connection }) => {
+    sock.ev.on("connection.update", ({ connection, lastDisconnect }) => {
 
-if(connection === "open"){
-console.log("WHATSAPP CONNECTED")
-}
+        if(connection === "open"){
+            console.log("WHATSAPP CONNECTED")
+        }
 
-})
+        if(connection === "close"){
+            console.log("CONNECTION CLOSED")
+        }
 
-/* ===== PAIRING CODE ===== */
+    })
 
-if(!sock.authState.creds.registered){
-
-const phoneNumber = "628xxxxxxxxxx" // ganti nomor WA anda
-
-const code = await sock.requestPairingCode(phoneNumber)
-
-console.log("PAIRING CODE ANDA:")
-console.log(code)
-
-}
-
-return sock
+    return sock
 }
 
 module.exports = startWhatsApp
